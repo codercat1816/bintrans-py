ae89950634694a9aa71691cb5aa28b9341f6ec70
Hackercat1826 <165159391+Hackercat1826@users.noreply.github.com>
2024-12-12 20:00:59 +0000

    Create dtobin-upgraded.py
    plan to make it endless as in can use over and over again

---
+++ dtobin-upgraded.py
@@ -0,0 +1,10 @@
+
+# Decimal work
+decimal_request = input("Decimal to Binary: ") 
+decimal_request = int(decimal_request) 
+
+# Binary work
+binary_value = bin(decimal_request)
+
+# Printing the binary value
+print(binary_value[2:])
